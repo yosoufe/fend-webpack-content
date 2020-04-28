6912bfec444f0b50d5a5c5e9ce45b6da2d6c3823
Yousof <yousofebneddin@yahoo.com>
2020-04-27 23:52:19 -0700

    entry point

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  // changing the default folder of entry
+  // from here webpack starts to build the dependency tree
+  entry: `./src/client/index.js`, 
+};
