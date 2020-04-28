74b5f5b17b2135eee41a0b681b83246821d2e035
Yousof <yousofebneddin@yahoo.com>
2020-04-28 00:25:02 -0700

    babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,13 @@ module.exports = {
   // changing the default folder of entry
   // from here webpack starts to build the dependency tree
   entry: `./src/client/index.js`, 
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
