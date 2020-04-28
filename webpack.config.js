c355e5f91615202f19496dbbd912a54b9d936141
Yousof <yousofebneddin@yahoo.com>
2020-04-28 00:36:51 -0700

    use html plugin to auto copy the html file into dist folder and remove hard coded paths to scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin'); // reference to plugin
 
 module.exports = {
   // changing the default folder of entry
@@ -8,10 +9,19 @@ module.exports = {
   module: {
     rules: [
       {
+        // loader to be able to use es6 in browser side
         test: '/\.js$/',
         exclude: /node_modules/,
         loader: "babel-loader"
       }
     ]
-  }
+  },
+  plugins:[
+    // plugin to copy html files into dist folder
+    // it also adds the js file into the html file in the dis folder
+    new HtmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
